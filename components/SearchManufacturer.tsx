b5cc6f8cd6ef67473360b107d1a50b7607f710ba
yossiSoudry <121138149+yossiSoudry@users.noreply.github.com>
2023-06-19 14:26:41 +0300

    2

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,7 +3,7 @@ import { manufacturers } from "@/constants";
 import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
 import { Fragment, useState } from "react";
-import { CheckIcon } from '@heroicons/react/20/solid'
+import { CheckIcon } from "@heroicons/react/20/solid";
 import { SearchManuFacturerProps } from "@/types";
 
 const SearchManufacturer = ({
@@ -15,65 +15,75 @@ const SearchManufacturer = ({
     query === ""
       ? manufacturers
       : manufacturers.filter((item) =>
-          item
+          item[1]
             .toLowerCase()
             .replace(/\s+/g, "")
             .includes(query.toLowerCase().replace(/\s+/g, ""))
         );
-  return (
-      <Combobox value={manufacturer} onChange={setManufacturer}>
-          <Combobox.Input
-            className="w-full h-[48px] pr-12 p-4 rounded-r-full max-sm:rounded-full bg-light-white outline-none text-sm"
-            placeholder="פולקסוואגן"
-            displayValue={(manufacturer: string) => manufacturer}
-            onChange={(e) => setQuery(e.target.value)}
-          ></Combobox.Input>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100 "
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            afterLeave={() => setQuery("")}
-          >
-            <Combobox.Options>
-            {filteredManufacturers.length === 0 && query !== "" ? (
-                <Combobox.Option
-                  value={query}
-                  className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
-                >
-                  צור חדש "{query}"
-                </Combobox.Option>
-              ) : (
-                filteredManufacturers.map((item) => (
-                  <Combobox.Option
-                    key={item}
-                    className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? "bg-primary-blue text-white" : "text-gray-900"
-                      }`
-                    }
-                    value={item}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <span className={` truncate center ${selected ? "font-medium" : "font-normal"}`}>
-                          {item}
-                          {selected && <CheckIcon className="h-4" />}
-                        </span>
 
-                        {/* Show an active blue background color if the option is selected */}
-                        {/* {selected ? (
-                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
-                          ></span>
-                        ) : null} */}
-                      </>
-                    )}
-                  </Combobox.Option>
-                ))
-              )}
-            </Combobox.Options>
-          </Transition>
-      </Combobox>
+  const getEnglishNameByHebrewName = (hebrewName: string): string | null => {
+    const matchingManufacturer = manufacturers.find(
+      (item) => item[1] === hebrewName
+    );
+    return matchingManufacturer ? matchingManufacturer[0] : null;
+  };
+
+  return (
+    <Combobox value={manufacturer} onChange={setManufacturer}>
+      <Combobox.Input
+        className="w-full h-[48px] pr-12 p-4 rounded-r-full max-sm:rounded-full bg-light-white outline-none text-sm"
+        placeholder="פולקסוואגן..."
+        displayValue={(manufacturer: string) => manufacturer}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Transition
+        as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+        leave="transition ease-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        afterLeave={() => setQuery("")} // Reset the search query after the transition completes
+      >
+        <Combobox.Options
+          className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+          static
+        >
+          {filteredManufacturers.length === 0 && query !== "" ? (
+            <Combobox.Option
+              value={query}
+              className="cursor-default select-none py-2 pr-10 pl-4"
+            >
+              צור חדש "{query}"
+            </Combobox.Option>
+          ) : (
+            filteredManufacturers.map((item) => (
+              <Combobox.Option
+                key={item[0]}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pr-10 pl-4 ${
+                    active ? "bg-primary-blue text-white" : "text-gray-900"
+                  }`
+                }
+                value={getEnglishNameByHebrewName(item[1]) || ""}
+              >
+                {({ selected, active }) => (
+                  <>
+                    <span
+                      className={` truncate center ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {item[1]}
+                      {selected && <CheckIcon className="h-4" />}
+                    </span>
+                    {/* ... */}
+                  </>
+                )}
+              </Combobox.Option>
+            ))
+          )}
+        </Combobox.Options>
+      </Transition>
+    </Combobox>
   );
 };
 
